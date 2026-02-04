1f8ced2af4288831d3a35fcb9317da1fa46e5435
Nuclea88 <ingrid.lopov@gmail.com>
2026-02-04 17:15:44 +0100

    feat: new models class with getters, setters, equals and hashcode

--- src/main/java/com/library/model/Book.java
+++ src/main/java/com/library/model/Book.java
@@ -88,7 +88,63 @@ public class Book {
     }
 
     //equals y HashCode
-    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
+        result = prime * result + ((isbn == null) ? 0 : isbn.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((publicationDate == null) ? 0 : publicationDate.hashCode());
+        result = prime * result + ((created_at == null) ? 0 : created_at.hashCode());
+        result = prime * result + ((updated_at == null) ? 0 : updated_at.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Book other = (Book) obj;
+        if (id != other.id)
+            return false;
+        if (title == null) {
+            if (other.title != null)
+                return false;
+        } else if (!title.equals(other.title))
+            return false;
+        if (isbn == null) {
+            if (other.isbn != null)
+                return false;
+        } else if (!isbn.equals(other.isbn))
+            return false;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        if (publicationDate == null) {
+            if (other.publicationDate != null)
+                return false;
+        } else if (!publicationDate.equals(other.publicationDate))
+            return false;
+        if (created_at == null) {
+            if (other.created_at != null)
+                return false;
+        } else if (!created_at.equals(other.created_at))
+            return false;
+        if (updated_at == null) {
+            if (other.updated_at != null)
+                return false;
+        } else if (!updated_at.equals(other.updated_at))
+            return false;
+        return true;
+    }
 
 
 }
